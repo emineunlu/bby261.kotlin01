6e0d7fa704a6098d3bf789ae7c6120a3944cbec4
Emine ünlü <emineunlu223@gmail.com>
2017-11-10 14:15:17 +0300

    Burcsozlugu

---
+++ app/src/main/java/com/example/bby/hacettepe/burcsozlugu/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.bby.hacettepe.burcsozlugu
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        loadWebpage()
+            }
+
+       fun loadWebpage() {
+
+               // Enable javascript
+               webview.settings.javaScriptEnabled = true
+           webview.loadUrl("http://yunus.hacettepe.edu.tr/~emineunlu/projex/")
+    }
+}
